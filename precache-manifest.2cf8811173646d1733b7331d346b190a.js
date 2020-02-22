f894a31886793a2d3514fd211cba42b8996cdafa
Zeen Lam <zeen@Zeens-MacBook-Pro.local>
2020-02-22 13:56:45 +0000

    Updates

---
+++ precache-manifest.2cf8811173646d1733b7331d346b190a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39c5d5a1e814c9b8d2caea5241aa1ce4",
+    "url": "/User-Profiles/index.html"
+  },
+  {
+    "revision": "2c78008d817e91144bdf",
+    "url": "/User-Profiles/static/css/main.c4a22636.chunk.css"
+  },
+  {
+    "revision": "d99a10a7d8fc43f5a65f",
+    "url": "/User-Profiles/static/js/2.34359ed4.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/User-Profiles/static/js/2.34359ed4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2c78008d817e91144bdf",
+    "url": "/User-Profiles/static/js/main.1843c03c.chunk.js"
+  },
+  {
+    "revision": "68d55ee523717d2d0846",
+    "url": "/User-Profiles/static/js/runtime-main.e6b14e4c.js"
+  }
+]);
